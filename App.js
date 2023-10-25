4da3e56ff0b8f5f4ef09ec30246e9519abf3abf6
bakuyy <sophieyang12345@gmail.com>
2023-10-25 13:50:45 -0400

    navigation

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MainContainer from './navigation/MainContainer';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <MainContainer/>
+      <Text>Hello, World!</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ navigation/screens/Profile.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Text, View} from 'react-native'
+
+export default function Profile({navigation}) {
+  return (
+    <div>
+        <VIew>
+            <Text onPress={()=> navigation.navigate("Profile")}>
+            
+            </Text>
+        </VIew>
+      
+    </div>
+  )
+}
